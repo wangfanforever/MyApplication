c91a239568d60e122eda34c82160d80f4791f2b3
wang <415189943@qq.com>
2015-10-27 17:32:49 +0800

    测试

--- app/src/main/java/com/example/wang/myapplication/MainActivity.java
+++ app/src/main/java/com/example/wang/myapplication/MainActivity.java
@@ -93,7 +93,7 @@ public class MainActivity extends AppCompatActivity
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        int id2 = item.getItemId();
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
